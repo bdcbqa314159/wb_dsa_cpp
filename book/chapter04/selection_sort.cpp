43c05d104f25d6ae50355de7fc9c9c3c7df88435
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2025-05-21 09:26:19 +0100

    correcting errors

--- book/chapter04/selection_sort.cpp
+++ book/chapter04/selection_sort.cpp
@@ -2,40 +2,32 @@
 
 using namespace std;
 
-void InsertionSort(int arr[], int arrSize) {
-  for (int i = 0; i < arrSize; ++i) {
-    int refValue = arr[i];
+void SelectionSort(int arr[], int arrSize) {
+  int minIndex{};
+  for (int i = 0; i < arrSize - 1; ++i) {
+    minIndex = i;
 
-    int j{};
-
-    for (j = i - 1; j >= 0; --j) {
-      if (arr[j] > refValue) {
-        arr[j + 1] = arr[j];
-      } else {
-        break;
+    for (int j = i + 1; j < arrSize; ++j) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
       }
     }
-
-    arr[j + 1] = refValue;
+    swap(arr[i], arr[minIndex]);
   }
 }
 
 int main() {
-  cout << "Insertion Sort" << endl;
+  cout << "Selection Sort" << endl;
 
-  // Initialize a new array
   int arr[] = {43, 21, 26, 38, 17, 30};
   int arrSize = sizeof(arr) / sizeof(*arr);
 
-  // Display the initial array
   cout << "Initial array: ";
   for (int i = 0; i < arrSize; ++i) cout << arr[i] << " ";
   cout << endl;
 
-  // Sort the array with InsertionSort algorithm
-  InsertionSort(arr, arrSize);
+  SelectionSort(arr, arrSize);
 
-  // Display the sorted array
   cout << "Sorted array : ";
   for (int i = 0; i < arrSize; ++i) cout << arr[i] << " ";
   cout << endl;
