e00e3842ec593e24fd4e928cb82a952a22dbdfc5
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2025-05-27 06:08:56 +0100

    adding interpolation search

---
+++ book/chapter05/interpolation_search.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int InterpolationSearch(int arr[], int lowIndex, int highIndex, int val) {
+  if (lowIndex <= highIndex) {
+    int middleIndex =
+        lowIndex + ((val - arr[lowIndex]) * (highIndex - lowIndex) /
+                    (arr[highIndex] - arr[lowIndex]));
+
+    if (arr[middleIndex] == val) {
+      return middleIndex;
+    }
+
+    else if (arr[middleIndex] > val) {
+      return InterpolationSearch(arr, lowIndex, middleIndex - 1, val);
+    }
+
+    else {
+      return InterpolationSearch(arr, middleIndex + 1, highIndex, val);
+    }
+  }
+
+  return -1;
+}
+
+int main() {
+  cout << "Interpolation Search" << endl;
+
+  int arr[] = {3, 8, 11, 15, 16, 23, 28, 30, 32, 39, 42, 44, 47, 48, 50};
+  int arrSize = sizeof(arr) / sizeof(*arr);
+
+  int searchedValue = 16;
+
+  int i = InterpolationSearch(arr, 0, arrSize - 1, searchedValue);
+
+  if (i != -1) {
+    cout << searchedValue << " is found in index ";
+    cout << i << endl;
+  } else {
+    cout << "Could not find value " << searchedValue;
+    cout << endl;
+  }
+
+  return 0;
+}
